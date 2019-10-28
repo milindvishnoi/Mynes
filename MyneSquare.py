25694ccefb2b7e471c274e65608434e99107380b
Justin Paglia <paglia.justin23@gmail.com>
2019-10-27 20:17:52 -0400

    Re-push of my previous work, but with the right account this time.

---
+++ MyneSquare.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class MyneSquare:
+    """
+    An object that acts as a playable space on the board.
+    """
+
+    def __init__(self, value: int, flag: bool, icon: str, hitbox: pygame.rect):
+        """
+        Creates a new square object for the board.
+        :param value: Integer that tells Mynes what is on this square.
+        -1 is a Mine, 0 is an empty space, [1,8] are spaces with adjacent mines
+        :param flag: Boolean attribute for if a flag is placed on top of this square.
+        """
+        self.value = value
+        self.flag = flag
+        self.icon = pygame.image.load(icon)
+        self.hitbox = hitbox
